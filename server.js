0ca14a7ac75f676927724aeb16df8928252e2072
Hamad-Alabri <h.m.k.s.alabri@edu.salford.ac.uk>
2024-04-28 19:59:46 +0300

    Add root route handler for health checking

--- server.js
+++ server.js
@@ -10,34 +10,37 @@ const userRoutes = require('./routes/user');
 const app = express();
 
 // CORS Configuration
+// Ensure the origin matches the URL of your deployed frontend or is open for development.
 const corsOptions = {
-    origin: process.env.FRONTEND_URL,  // This should be the URL of your frontend application
+    origin: process.env.FRONTEND_URL,  // Use environment variable to configure CORS dynamically
     optionsSuccessStatus: 200
 };
 
 // Enable CORS with options
 app.use(cors(corsOptions));
 
-// Middleware
+// Middleware to parse JSON bodies
 app.use(express.json());
+
+// Simple logger to show the methods and paths of requests
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
 
-// Routes
+// API routes
 app.use('/api/credentials', credentialRoutes);
 app.use('/api/user', userRoutes);
 
-// Root Route for simple health checking
+// Root route for basic API health check
 app.get('/', (req, res) => {
     res.send('Password Manager API is running');
 });
 
-// MongoDB connection
+// Connect to MongoDB and start the server
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        // Listen for requests only after the database connection is successful
+        // Listen for requests on the port provided by the environment
         app.listen(process.env.PORT, () => {
             console.log(`Connected to db & listening on port ${process.env.PORT}`);
         });
@@ -46,7 +49,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
         console.error('Error connecting to MongoDB:', error.message);
     });
 
-// Default error handler
+// Default error handler to catch any unhandled errors
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
