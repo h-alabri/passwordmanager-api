cf354c54390062bb01da156e45c4bdbb0032a74a
Hamad-Alabri <h.m.k.s.alabri@edu.salford.ac.uk>
2024-04-29 01:10:03 +0300

    Update server configuration and add CORS support

--- server.js
+++ server.js
@@ -1,56 +1,33 @@
-require('dotenv').config();
-
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const credentialRoutes = require('./routes/credentials');
 const userRoutes = require('./routes/user');
 
-// Express app
+// express app
 const app = express();
 
-// CORS Configuration
-// Ensure the origin matches the URL of your deployed frontend or is open for development.
-const corsOptions = {
-    origin: process.env.FRONTEND_URL,  // Use environment variable to configure CORS dynamically
-    optionsSuccessStatus: 200
-};
-
-// Enable CORS with options
-app.use(cors(corsOptions));
-
-// Middleware to parse JSON bodies
+// middleware
 app.use(express.json());
+app.use(cors()); // Add this line to enable CORS for all routes
 
-// Simple logger to show the methods and paths of requests
 app.use((req, res, next) => {
-    console.log(req.path, req.method);
-    next();
+  console.log(req.path, req.method);
+  next();
 });
 
-// API routes
+// routes
 app.use('/api/credentials', credentialRoutes);
 app.use('/api/user', userRoutes);
 
-// Root route for basic API health check
-app.get('/', (req, res) => {
-    res.send('Password Manager API is running');
-});
-
-// Connect to MongoDB and start the server
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        // Listen for requests on the port provided by the environment
-        app.listen(process.env.PORT, () => {
-            console.log(`Connected to db & listening on port ${process.env.PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error('Error connecting to MongoDB:', error.message);
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('connected to db & listening on port', process.env.PORT);
     });
-
-// Default error handler to catch any unhandled errors
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
+  })
+  .catch((error) => {
+    console.log(error);
+  });
